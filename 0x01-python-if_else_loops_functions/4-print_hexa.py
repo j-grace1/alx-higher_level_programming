17ab68c0dd06b1cb525666e80cac1a73cf72b3bb
j-grace1 <nchindejoshoua@gmail.com>
2022-09-21 00:47:33 +0100

    prints 0 to 100

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,7 @@
-#!/usr/bin/python3
-for i in range(0, 99):
-    print('{} = {}'.format(i, hex(i)))
+
+for i in range(0, 100):
+    if i < 99:
+        print('{:02d}'.format(i), end= ', ')
+    else:
+        print(i)
+    
