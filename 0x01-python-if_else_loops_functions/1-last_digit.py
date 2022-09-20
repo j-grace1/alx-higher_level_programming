8959ee18715f21b15cfdd23967a662b3c3b40612
j-grace1 <nchindejoshoua@gmail.com>
2022-09-20 22:35:28 +0100

    prints last digit of randomly generated number and commments on it

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last = number[-1]
+if number == 0:
+    print(f"Last digit of {number} is {last} and is 0")
+elif number < 6:
+    print(f"Last digit of {number} is {last} and is less than 6 and not 0")
+else:
+    print(f"Last digit of {number} is {last} and is greater than 5")
+
