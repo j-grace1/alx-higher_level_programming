34a80f7c213ad598079f4c2330f7537d5c2efa67
j-grace1 <nchindejoshoua@gmail.com>
2022-09-21 03:47:48 +0100

    removes a single character from a string

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,14 @@
+from operator import index
+
+
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    new_str = ''
+    x = str.index(str[n])
+    for i in range(0, len(str)):
+        if i != x:
+            new_str = new_str + str[i]
+        else:
+            continue
+    print('{}'.format(new_str))
+remove_char_at('hello boss man', -7)
