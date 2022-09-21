321f2832f3b442bee98c18c4b370805db1aeba78
j-grace1 <nchindejoshoua@gmail.com>
2022-09-21 03:28:04 +0100

    print from 1 to 100 replacing multiples of 3 by fizz, 5 by buzz and 15 by fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i >= 3 and i % 3 == 0 and i % 15 != 0:
+            print("{}".format('Fizz'), end= ' ')
+        elif i >= 5 and i % 5 == 0 and i % 15 != 0:
+            print("{}".format('Buzz'), end= ' ')
+        elif i >= 15 and i % 15 == 0:
+            print("{}".format('FizzBuzz'), end= ' ')
+        else:
+            print("{}".format(i), end= ' ')
