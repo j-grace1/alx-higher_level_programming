20b832f18ec6d45995b6cf69a9ca739bacc4bb62
j-grace1 <nchindejoshoua@gmail.com>
2022-09-21 03:29:15 +0100

    removed some irrelevant space

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,10 +2,10 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i >= 3 and i % 3 == 0 and i % 15 != 0:
-            print("{}".format('Fizz'), end= ' ')
+            print("{}".format('Fizz'), end=' ')
         elif i >= 5 and i % 5 == 0 and i % 15 != 0:
-            print("{}".format('Buzz'), end= ' ')
+            print("{}".format('Buzz'), end=' ')
         elif i >= 15 and i % 15 == 0:
-            print("{}".format('FizzBuzz'), end= ' ')
+            print("{}".format('FizzBuzz'), end=' ')
         else:
-            print("{}".format(i), end= ' ')
+            print("{}".format(i), end=' ')
