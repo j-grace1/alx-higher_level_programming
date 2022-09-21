30b2cfa1b7e998bc36f7f7977a513397d7f4f43d
j-grace1 <nchindejoshoua@gmail.com>
2022-09-21 03:01:04 +0100

    removed sign from output

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        sign = "-"
-    else:
-        sign = ''
     str_numb = str(number)
     last_digit = str_numb[-1]
-    print("{}{}".format(sign, last_digit))
+    print("{}".format(last_digit))
