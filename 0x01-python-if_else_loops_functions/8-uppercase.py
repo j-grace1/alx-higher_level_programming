b8b7553cd72c3933ba4964ae0968b2238f7af740
j-grace1 <nchindejoshoua@gmail.com>
2022-09-21 02:37:12 +0100

    converts to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(0, len(str)):
+        x = ord(str[i])
+        if x < 124 and x > 96:
+            y = x - 97
+            new_str = chr(y + 65)
+            print("{}".format(new_str), end='')
+        else:
+            new_str = str[i]
+            print("{}".format(new_str), end='')
+
+
