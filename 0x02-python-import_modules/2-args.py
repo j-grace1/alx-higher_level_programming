353d893ae19781208fff80d4860a7d9001b5b2d3
j-grace1 <nchindejoshoua@gmail.com>
2022-09-26 21:28:17 +0100

    hello world

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-str = str(input(''))
-split_str = str.split(' ')
-argv = []
-for i in split_str:
-    argv.append(i)
-argc = len(argv)
-if argc > 0:
+import sys
+argc = len(sys.argv)
+if argc > 1:
     print('{} arguments:'.format(argc))
     for i in range(0, argc):
-        print('{}: {}'.format(i, argv[i]))
-elif argc == 0:
+        print('{}: {}'.format(i, sys.argv[i]))
+elif argc == 1:
     print('{} argument:'.format(argc))
 else:
     print('{} arguments.'.format(argc))
