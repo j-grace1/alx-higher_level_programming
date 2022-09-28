e4c973aa8e37fd1ae2341d6c183a92da8c072b8a
j-grace1 <nchindejoshoua@gmail.com>
2022-09-28 21:23:59 +0100

    changed logic of my code

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 # 3-common_elements.py
 def common_elements(set_1, set_2):
-    new_set = ()
-    for i in set_1:
-        for j in set_2:
-            if i == j:
-                new_set.append(i)
+    new_set = (set_1 & set_2)
     return new_set
