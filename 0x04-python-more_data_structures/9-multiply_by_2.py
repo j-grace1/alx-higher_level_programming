e7338e29a8ec336ec87ab23692be6c05efeb3c77
j-grace1 <nchindejoshoua@gmail.com>
2022-09-28 23:38:01 +0100

    added shebang and comment

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
+# 9-multiply_by_2.py
 def multiply_by_2(a_dictionary):
     new_dict = {}
     for i in a_dictionary:
-        new_dict.update({i: (a_dictionary[i])**2})
+        new_dict.update({i: (a_dictionary[i])*2})
     return(new_dict)
