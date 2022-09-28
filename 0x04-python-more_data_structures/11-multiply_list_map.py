2db658f17bd5f795dc5a2f058d5cf4a2e6583e47
j-grace1 <nchindejoshoua@gmail.com>
2022-09-29 00:43:55 +0100

    little change

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # 10-best_score.py
 def multiply_list_map(my_list=[], number=0):
-    print(list(map(lambda x: x* number, my_list)))
+    print(list(map(lambda x: x*number, my_list)))
