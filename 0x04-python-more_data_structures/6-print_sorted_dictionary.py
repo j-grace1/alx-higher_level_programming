eab43586a02e25f25ea1b2dd64418e065e35b69b
j-grace1 <nchindejoshoua@gmail.com>
2022-09-28 22:15:32 +0100

    changed return to print

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,4 +4,4 @@ def print_sorted_dictionary(a_dictionary):
     new_dict = {}
     for i in sorted(a_dictionary):
         new_dict.update({i: a_dictionary[i]})
-    return(new_dict)
+    print(new_dict)
