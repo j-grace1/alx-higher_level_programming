24bce87191937c7dc28abc5639f80710062f3892
j-grace1 <nchindejoshoua@gmail.com>
2022-09-28 23:20:47 +0100

    chanded dey variable to be deleted

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,5 +3,5 @@
 def simple_delete(a_dictionary, key=""):
     for i in a_dictionary:
         if i == key:
-            del a_dictionary[key]
+            del a_dictionary[i]
     return(a_dictionary)
