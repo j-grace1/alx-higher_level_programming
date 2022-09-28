42d8a3c89d69dfe3c377098cdf1a702be1388533
j-grace1 <nchindejoshoua@gmail.com>
2022-09-28 23:49:51 +0100

    reduced code complexity

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,6 @@
 # 10-best_score.py
 def best_score(a_dictionary):
     if len(a_dictionary) > 0:
-        score_list = []
-        for i in a_dictionary:
-            score_list.append(a_dictionary[i])
-        y = sorted(score_list)[-1]
-        return(y)
+        return(sorted(a_dictionary.values)[-1])
     else:
         return None
