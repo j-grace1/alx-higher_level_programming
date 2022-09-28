3bce2731d04f59c58fd0199bbe3c368ad28546b1
j-grace1 <nchindejoshoua@gmail.com>
2022-09-28 23:06:59 +0100

    codditioned ma code

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -4,5 +4,9 @@ from re import A
 #!/usr/bin/python3
 # 8-simple_delete.py
 def simple_delete(a_dictionary, key=""):
-    del a_dictionary[key]
+    for i in a_dictionary:
+        if i == key:
+            del a_dictionary[key]
+        else:
+            pass
     return(a_dictionary)
